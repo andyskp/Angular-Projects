77c678dabdfcb76b50709313fd8a789a53cfdba9
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-17 16:34:21 -0500

    Se realizan procesos de cambios de estados usando signals en angular en el counter-page.component

--- src/app/Pages/counter/counter-page.component.ts
+++ src/app/Pages/counter/counter-page.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-
+import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -9,21 +8,25 @@ import { Component } from "@angular/core";
     padding: 10px;
     margin: 5px 10px;
   }
-  `
+  `,
 })
-
-
 export class CounterPageComponent {
-  couter:number = 0;
+  couter:number = 1;
+  //? Trabajar con señales  (Signals) se importa
+  counterSignel = signal(10);
 
   increaseBy( value:number) {
     this.couter += value;
+    //! Se modifica el valor de la señal
+    this.counterSignel.update( (current) =>  current + value);
   }
   decrementBy(value:number) :void {
-    this.couter -= value
+    this.couter -= value;
+    this.counterSignel.update((current) => current - value);
   }
 
   resetCounter():void {
     this.couter = 0;
+    this.counterSignel.set(0);
   }
 }
