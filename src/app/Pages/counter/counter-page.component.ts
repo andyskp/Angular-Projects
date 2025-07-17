8bccd0b9c4dcc0c27388b90ce5d25b09ac0daf57
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-17 17:27:54 -0500

    Se agrega nuevo elemento css al componente counter manualmente

--- src/app/Pages/counter/counter-page.component.ts
+++ src/app/Pages/counter/counter-page.component.ts
@@ -2,30 +2,24 @@ import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
-  styles: `
-  button {
-    justify-content: center;
-    padding: 10px;
-    margin: 5px 10px;
-  }
-  `,
+  styleUrls: ['./counter-page.component.css']
 })
 export class CounterPageComponent {
-  couter:number = 1;
+  couter: number = 1;
   //? Trabajar con señales  (Signals) se importa de angular Core
   counterSignel = signal(10);
 
-  increaseBy( value:number) {
+  increaseBy(value: number) {
     this.couter += value;
     //! Se modifica el valor de la señal
-    this.counterSignel.update( (current) =>  current + value);
+    this.counterSignel.update((current) => current + value);
   }
-  decrementBy(value:number) :void {
+  decrementBy(value: number): void {
     this.couter -= value;
     this.counterSignel.update((current) => current - value);
   }
 
-  resetCounter():void {
+  resetCounter(): void {
     this.couter = 0;
     this.counterSignel.set(0);
   }
