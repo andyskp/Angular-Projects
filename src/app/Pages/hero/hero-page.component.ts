209a521b5d2cb16d1379f5131affe55d539c9e6e
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-18 10:23:54 -0500

    Se comentan cada uno de las funciones y sus datos

--- src/app/Pages/hero/hero-page.component.ts
+++ src/app/Pages/hero/hero-page.component.ts
@@ -9,19 +9,22 @@ import { Component, signal } from "@angular/core";
 })
 
 export class HeroPageComponent {
+      //? Se Dejan los valores por defecto de los Signals
      name = signal('Iron Man');
      age = signal(47);
-
+      //! Se concatena para mostrar los resultados
      getHeroDescription() {
       return `${this.name()} - ${this.age()}`
      }
-
+     //*TODO: Cambia el nombre de (name)
      changeHero() {
       this.name.set('Spiderman');
      }
+     //*TODO: Cambia el nombre de (age)
      changeAge() {
       this.age.set(60);
      }
+     //*TODO: Realiza un reset de los datos de la signals
      resetForm() {
       this.name.set('Iron Man');
       this.age.set(47);
