a0d39d14e5cad090f1d4aaa47f7c64633ceaaa2a
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-18 16:00:31 -0500

    Se crea un nuevo componente en Pages para agregarlo a las rutas del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './Pages/counter/counter-page.component';
 import { HeroPageComponent } from './Pages/hero/hero-page.component';
+import { DragonballComponent } from './Pages/dragonball/dragonball.component';
 
 export const routes: Routes = [
       {
@@ -10,6 +11,10 @@ export const routes: Routes = [
       {
             path: 'hero',
              component: HeroPageComponent
+      },
+      {
+            path: 'dragonball',
+            component: DragonballComponent
       },
       //? En caso de que falle vuelve al path vacio
       {
