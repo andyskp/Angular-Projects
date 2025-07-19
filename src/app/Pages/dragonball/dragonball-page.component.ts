53731e27b91c613a7a3be0baa47874e1728afcb7
Andres bustos <effectsxfighter@gmail.com>
2025-07-18 20:34:22 -0500

    Se Utiliza el class o ngClass para modificar estilos de forma condicional

--- src/app/Pages/dragonball/dragonball-page.component.ts
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 //? Interface para tipar el objeto
 interface Character {
@@ -10,15 +11,24 @@ interface Character {
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
+  imports: [
+    // NgClass
+
+  ],
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
   //! Se concatena para mostrar los resultados
     characters = signal<Character[]>([
-      {id:1, name:"Goku", power:9000},
+      {id:1, name:"Goku", power:9001},
       {id:2, name:"Vegeta", power:7500},
       {id:3, name:"Piccolo", power:7000},
       {id:4, name:"Trunks", power:5000},
     ]);
+
+    // powerClasses = computed(() => {
+    //   return {
+    //     'text-danger':true,
+    //   }
+    // })
 }
