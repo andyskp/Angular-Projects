654be09586b7afa4a7f68a36b12ee0bf58ead541
Andres bustos <effectsxfighter@gmail.com>
2025-07-18 20:18:32 -0500

    Se utiliza @for en el html del elemento para iterar los atributos y valores de un Array, se realiza la interpolacion del mismo para mostrarlas

--- src/app/Pages/dragonball/dragonball-page.component.ts
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+//? Interface para tipar el objeto
+interface Character {
+  id:number;
+  name:string;
+  power:number;
+};
+
 
 @Component({
   selector: 'app-dragonball',
@@ -6,5 +14,11 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-
+  //! Se concatena para mostrar los resultados
+    characters = signal<Character[]>([
+      {id:1, name:"Goku", power:9000},
+      {id:2, name:"Vegeta", power:7500},
+      {id:3, name:"Piccolo", power:7000},
+      {id:4, name:"Trunks", power:5000},
+    ]);
 }
