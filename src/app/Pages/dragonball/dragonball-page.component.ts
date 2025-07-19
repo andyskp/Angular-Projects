dc931cf2492c66ed35ee28a33f0e7850cafca485
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-19 08:08:15 -0500

    Se adecuan ngClass para condicionar estilos

--- src/app/Pages/dragonball/dragonball-page.component.ts
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id:number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
@@ -6,5 +12,10 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-
+  characters = signal<Character[]>([
+    { id: 1, name: "Goku", power: 9001 },
+    { id: 2, name: "Vegeta", power: 8000 },
+    { id: 3, name: "Piccolo", power: 3000 },
+    { id: 4, name: "Trunks", power: 1000 },
+  ]);
 }
