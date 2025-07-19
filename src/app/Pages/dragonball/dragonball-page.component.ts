f883dfba782032432b7189912010a4d351199e88
ProgramadorAzteca04 <programadorazteca04@gmail.com>
2025-07-19 08:10:53 -0500

    Se realiza un merge con nueva logica

--- src/app/Pages/dragonball/dragonball-page.component.ts
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { Component, signal } from '@angular/core';
-
-interface Character {
-  id:number;
-  name: string;
-  power: number;
-}
-=======
 import { NgClass } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
@@ -17,7 +8,6 @@ interface Character {
   power:number;
 };
 
->>>>>>> 53731e27b91c613a7a3be0baa47874e1728afcb7
 
 @Component({
   selector: 'app-dragonball',
@@ -28,14 +18,6 @@ interface Character {
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-<<<<<<< HEAD
-  characters = signal<Character[]>([
-    { id: 1, name: "Goku", power: 9001 },
-    { id: 2, name: "Vegeta", power: 8000 },
-    { id: 3, name: "Piccolo", power: 3000 },
-    { id: 4, name: "Trunks", power: 1000 },
-  ]);
-=======
   //! Se concatena para mostrar los resultados
     characters = signal<Character[]>([
       {id:1, name:"Goku", power:9001},
@@ -49,5 +31,4 @@ export class DragonballPageComponent {
     //     'text-danger':true,
     //   }
     // })
->>>>>>> 53731e27b91c613a7a3be0baa47874e1728afcb7
 }
