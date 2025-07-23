765208390a71e0fcf1ea52e3688cd46f2b5e2f8a
Andres Bustos <ProgramadorAzteca04@gmail.com>
2025-07-23 09:44:49 -0500

    Se ejecuta el localStorage usando effect para que ejecute tareas de forma automatica y guardando la informacion en el localStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,12 @@ export class DragonballService {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
   ]);
+ //? Effect para el localStorage - Se ejecuta automaticamente
+ //! Usar solamenete para que ejecute una sola tarea
+ saveToLocalStorage = effect(() => {
+  localStorage.setItem('characters', JSON.stringify(this.characters()));
+})
+
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character]);
